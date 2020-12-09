927f7d4d14fd8b92e34a851a840e099a2b2ffa7a
tuananh210798 <75671952+tuananh210798@users.noreply.github.com>
2020-12-09 13:40:35 +0700

    update 12/09/2020

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 
 // import React from 'react';
 import './App.scss';
+import ColorBox from './components/ColorBox';
 
 function App() {
   return (
     <div className="app">
       <h1>Nguyen Tuan Anh</h1>
+      <ColorBox />
     </div>
   );
 }
